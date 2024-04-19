bd82439a4939c6d8f54a27fa0d01162c3d3036d7
GaziRiad <ouhibimustapha36@gmail.com>
2024-04-19 17:21:20 +0100

    Defining component props types

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,13 @@
+import CourseGoal from "@/components/CourseGoal";
 import React from "react";
 
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <main>
+      <CourseGoal
+        title="Lean React + Ts"
+        description="Learn it from the ground up"
+      />
+    </main>
+  );
 }
---
+++ components/CourseGoal.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function CourseGoal({
+  title,
+  description,
+}: {
+  title: string;
+  description: string;
+}) {
+  return (
+    <article>
+      <div>
+        <h2>{title}</h2>
+        <p>{description}</p>
+      </div>
+      <button>Delete</button>
+    </article>
+  );
+}
