2b472491593f33199bbbbc08e7b418de84be9b11
GaziRiad <ouhibimustapha36@gmail.com>
2024-04-20 10:14:13 +0100

    Another way of typing components

--- components/CourseGoal.tsx
+++ components/CourseGoal.tsx
@@ -18,3 +18,16 @@ export default function CourseGoal({ title, children }: CourseGoalProps) {
     </article>
   );
 }
+
+// const CourseGoal: FC<CourseGoalProps> = ({ title, children }) => {
+//   return (
+//     <article>
+//       <div>
+//         <h2>{title}</h2>
+//         {children}
+//       </div>
+//       <button>Delete</button>
+//     </article>
+//   );
+// };
+// export default CourseGoal;
